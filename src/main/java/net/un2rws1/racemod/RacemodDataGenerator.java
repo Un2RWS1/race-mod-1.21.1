3c8e6ad0b3467be07cb5f24abbe9dc4e0bf528bb
Kevin <lazykevin04@gmail.com>
2026-03-15 17:44:37 -0700

    Initial commit

---
+++ src/main/java/net/un2rws1/racemod/RacemodDataGenerator.java
@@ -0,0 +1,11 @@
+package net.un2rws1.racemod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RacemodDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
